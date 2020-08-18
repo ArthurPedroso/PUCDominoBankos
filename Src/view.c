c26184418cb9041503c40283e12044fa648c3bce
Mauricio <mm.villarnobo@gmail.com>
2020-08-18 18:29:20 -0300

    esqueleto do view

--- Src/view.c
+++ Src/view.c
@@ -1,21 +1,33 @@
 #include "view.h"
 #include <stdio.h>
 
-
-void printBoard()
+void menuInit() //inica menu
 {
-    for(int linIndx = 0; linIndx < 10; linIndx++)
-    {
-        for(int colIndx = 0; colIndx < 10; colIndx++)
-        {
-            printf("| 🁣🀱🁣");
-            printf("| 🁣🁣🁣");
-        }        
-        printf("\n");
-    }
+    printf("\n1- Play\n\n2- Quit");
 }
 
-void helloWorld(char* _msg)
+char menuInput() //recebe input do menu e returna
 {
-    printBoard(); 
+    char playerInput;
+
+    scanf("%c", &playerInput);
+    return playerInput;
+}
+
+void screenDisplay() //mostra a tela durante o jogo
+{
+    //recebe informacao do model de como esta o estado atual do jogo e imprime
+}
+
+void screenDisplayOptions() //mostra as opcoes do jogador
+{
+    printf("\n1- Place piece\n2- Buy piece\n3-Show screen\n4- Quit match");
+}
+
+char screenDisplayOptionsInput() //recebe input da opcao escolhida e o retona
+{
+    char playerInput;
+    
+    scanf("%c", &playerInput);
+    return playerInput;
 }
